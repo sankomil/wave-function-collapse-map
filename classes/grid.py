a31e7856634a497695623c7d783638b62dacec25
sankomil <43489998+sankomil@users.noreply.github.com>
2024-08-28 12:14:58 +0800

    feat(wfc): create grid and sprite classes

---
+++ classes/grid.py
@@ -0,0 +1,86 @@
+from collections import deque
+import random
+
+from heapq import heappush
+
+from classes.sprite import Sprite
+from rules import WIDTH, HEIGHT, NORTH, SOUTH, EAST, WEST
+
+
+class Grid:
+
+    def __init__(self, width=WIDTH, height=HEIGHT) -> None:
+        self.width = width
+        self.height = height
+        self.grid = []
+        self.entropy = []
+
+        for x in range(width):
+            row = []
+            for y in range(height):
+                sprite = Sprite(x, y)
+                row.append(sprite)
+                heappush(self.entropy, (sprite.entropy, sprite))
+            self.grid.append(row)
+        
+        for x in range(width):
+            for y in range(height):
+                sprite = self.grid[x][y]
+                if x > 0:
+                    sprite.neighbours[EAST] = self.grid[x-1][y]
+                if x < width - 1:
+                    sprite.neighbours[WEST] = self.grid[x+1][y]
+                if y > 0:
+                    sprite.neighbours[NORTH] = self.grid[x][y-1]
+                if y < height - 1:
+                    sprite.neighbours[SOUTH] = self.grid[x][y+1]
+        
+    
+    def get_lowest_entropy(self):
+        index = 0
+        while index < len(self.entropy) and self.entropy[index][0] == 0:
+            index += 1
+        return self.entropy[index][0]
+
+    def get_lowest_entropy_list(self):
+        ent = self.get_lowest_entropy()
+        index = 0
+        result = []
+
+        while index < len(self.entropy) and self.entropy[index][0] == ent:
+            result.append(self.entropy[index][1])
+            index += 1
+        
+        return result
+    
+    def wfc(self):
+        lowest_entropy = self.get_lowest_entropy_list()
+
+        if len(lowest_entropy) == 0:
+            return
+        
+        sprite_collapse = random.choice(lowest_entropy)
+        sprite_collapse.collapse()
+
+        stack = deque()
+        stack.append(sprite_collapse)
+
+        while len(stack) > 0:
+            sprite = stack.popleft()
+            pos = sprite.pos
+            directions = sprite.neighbours.keys()
+
+            for d in directions:
+                neighbour_sprite = sprite.neighbours[d]
+                if neighbour_sprite.entropy != 0:
+                    reduced = neighbour_sprite.reduce(d, pos)
+                    if reduced:
+                        stack.append(neighbour_sprite)
+        
+        self.entropy = []
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.grid[x][y].entropy != 0:
+                    heappush(self.entropy, (self.grid[x][y].entropy, self.grid[x][y]))
+    
+
---
+++ classes/sprite.py
@@ -0,0 +1,44 @@
+import random
+from rules import tile_neighbours
+
+class Sprite:
+
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+        self.pos = list(tile_neighbours.keys())
+        self.entropy = len(self.pos)
+        self.neighbours = dict()
+    
+
+    def collapse(self):
+        weights = [tile_neighbours[p]["TOTAL"] for p in self.pos]
+        self.pos = random.choices(self.pos, weights=weights, k=1)
+        self.entropy = 0
+    
+    def reduce(self, direction, neighbour_pos):
+        reduced = False
+
+        if self.entropy > 0:
+            neigh = []
+            for np in neighbour_pos:
+                neigh = neigh + list(tile_neighbours[np][direction].keys())
+            
+            checked = []
+            
+            for p in self.pos.copy():
+                for np in tile_neighbours[p][-direction].keys():
+                    if np not in neigh and np not in checked and np in self.pos:
+                        self.pos.remove(np)
+                        checked.append(np)
+                        reduced = True
+            
+            self.entropy = len(self.pos)
+        
+        return reduced
+    
+    def __lt__(self, other):
+        return self.entropy < other.entropy
+    
+    def __le__(self, other):
+        return self.entropy <= other.entropy
