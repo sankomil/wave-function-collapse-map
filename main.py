344a9dfcb0d47c729c8dc74a5673dc7f08b8e3a3
sankomil <43489998+sankomil@users.noreply.github.com>
2024-08-30 12:27:00 +0800

    feat: replace console print to drawing in pygame

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-from classes.grid import Grid
+import sys
+import pygame
+
+from classes import Grid, Draw, grid
+from rules import *
 
 
 if __name__ == '__main__':
-    test = Grid()
-    result = test.wfc()
+    pygame.init()
+    clock = pygame.time.Clock()
 
-    while result != 0:
+    display = pygame.display.set_mode((WIDTH * SPRITE_SIZE * SCALE, HEIGHT * SPRITE_SIZE * SCALE))
 
-        for x in range(test.width):
-            print("")
-            for y in range(test.height):
-                print(test.grid[x][y].entropy, "|", end=" ")
-        print("\n")
+    box = Grid()
+    draw = Draw(box)
+
+    draw.update()
+
+    is_running = True
+    finished = False
+
+    while is_running:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                is_running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    is_running = False
         
-        result = test.wfc()
+        if not finished:
+            result = box.wfc()
+            print(result)
+            if result == 0:
+                finished = True
+            
+        draw.update()
 
+
+        draw.draw(display)
+
+        pygame.display.flip()
+        clock.tick(60)
+    
+    pygame.quit()
